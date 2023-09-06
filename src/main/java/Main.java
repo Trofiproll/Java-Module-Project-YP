f52e5880b85431d6974666170d231c8f80ea06e6
Trofiproll <denispartola@gmail.com>
2023-09-06 16:20:16 +0300

    ДЗ

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Locale.setDefault(Locale.ENGLISH);
+        Scanner in = new Scanner(System.in);
+        System.out.println("На сколько человек нужно разделить счёт?");
+        int guestsNumber;
+        while(true) {
+            if (in.hasNextInt()) {
+                guestsNumber = in.nextInt();
+                if(guestsNumber > 1) break;
+                else System.out.println("Введено число меньше 2. Повторите попытку");
+            } else if (in.hasNext()) {
+                System.out.println("Некорректный ввод. Повторите попытку");
+                in.next();
+            }
+        }
+
+        Calculator bill = new Calculator(guestsNumber);
+        bill.addProducts();
+        bill.calcutate();
     }
 }
